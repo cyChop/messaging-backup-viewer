ebf96feecfe9324b9704c8f00a0cfe3ed7c5d671
Cyrille Chopelet <cyrille.chopelet@mines-nancy.org>
2015-11-09 19:52:56 +0100

    #8 POC closing application from tray icon

--- messaging-backup-viewer-app/src/main/java/org/keyboardplaying/messaging/ui/ApplicationManager.java
+++ messaging-backup-viewer-app/src/main/java/org/keyboardplaying/messaging/ui/ApplicationManager.java
@@ -17,19 +17,21 @@
 package org.keyboardplaying.messaging.ui;
 
 import java.awt.AWTException;
-import java.awt.CheckboxMenuItem;
 import java.awt.Image;
-import java.awt.Menu;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.keyboardplaying.messaging.ui.icon.IconSize;
 import org.keyboardplaying.messaging.ui.icon.ImageLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
 /**
@@ -39,6 +41,9 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ApplicationManager {
+
+    @Autowired
+    private ConfigurableApplicationContext context;
 
     private TrayIcon trayIcon;
 
@@ -87,28 +92,17 @@ public class ApplicationManager {
 
     private PopupMenu makeMenu() {
         // Create a pop-up menu components
-        MenuItem aboutItem = new MenuItem("About");
-        CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
-        CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
-        Menu displayMenu = new Menu("Display");
-        MenuItem errorItem = new MenuItem("Error");
-        MenuItem warningItem = new MenuItem("Warning");
-        MenuItem infoItem = new MenuItem("Info");
-        MenuItem noneItem = new MenuItem("None");
         MenuItem exitItem = new MenuItem("Exit");
+        exitItem.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                context.close();
+            }
+        });
 
         // Add components to pop-up menu
         PopupMenu popup = new PopupMenu();
-        popup.add(aboutItem);
-        popup.addSeparator();
-        popup.add(cb1);
-        popup.add(cb2);
-        popup.addSeparator();
-        popup.add(displayMenu);
-        displayMenu.add(errorItem);
-        displayMenu.add(warningItem);
-        displayMenu.add(infoItem);
-        displayMenu.add(noneItem);
         popup.add(exitItem);
         return popup;
     }
